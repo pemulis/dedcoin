cc9b0c36ebba56db09155491c0ea8346e7e0eb28
John Shutt <john.d.shutt@gmail.com>
2018-02-22 11:12:43 -0800

    change Dedcoin to be a MintableToken, and update language in package.json

--- contracts/Dedcoin.sol
+++ contracts/Dedcoin.sol
@@ -1,14 +1,8 @@
 pragma solidity ^0.4.18;
-import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
-contract Dedcoin is StandardToken {
+contract Dedcoin is MintableToken {
   string public name = "Dedcoin";
   string public symbol = "DED";
-  uint public decimals = 2;
-  uint public INITIAL_SUPPLY = 10000000000 * (10 ** decimals);
-
-  function Dedcoin() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-  }
+  uint8 public decimals = 18;
 }
