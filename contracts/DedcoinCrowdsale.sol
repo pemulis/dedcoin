6f514237c2c869c0d61111a3e807d2a142fc3155
John Shutt <john.d.shutt@gmail.com>
2018-02-22 15:43:56 -0800

    Add some code to DedcoinCrowdsale.sol and 3_deploy_crowdsale.js to fill in required variables for the Crowdsale constructor

--- contracts/DedcoinCrowdsale.sol
+++ contracts/DedcoinCrowdsale.sol
@@ -6,9 +6,10 @@ import './DecreasingPriceCrowdsale.sol';
 
 contract DedcoinCrowdsale is DecreasingPriceCrowdsale {
 
-  function DedcoinCrowdsale(uint256 _initialRate, uint256 _finalRate, uint256 _openingTime, uint256 _closingTime)
+  function DedcoinCrowdsale(uint256 _initialRate, uint256 _finalRate, uint256 _openingTime, uint256 _closingTime, uint256 _rate, address _wallet, MintableToken _token)
     DecreasingPriceCrowdsale(_initialRate, _finalRate)
-    TimedCrowdsale(_openingTime, _closingTime) {
+    TimedCrowdsale(_openingTime, _closingTime)
+    Crowdsale(_rate, _wallet, _token) {
   }
 
   // creates the token to be sold.
--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -1,10 +1,14 @@
 const DedcoinCrowdsale = artifacts.require("./DedcoinCrowdsale.sol")
+const Dedcoin = artifacts.require("./Dedcoin.sol")
 
 module.exports = function(deployer, network, accounts) {
   const initialRate = (1/30000000000000000)
   const finalRate = 1
   const openingTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
   const closingTime = openingTime + (86400 * 20) // 20 days
+  const rate = new web3.BigNumber(1000)
+  const wallet = accounts[0]
+  const token = Dedcoin
 
-  deployer.deploy(DedcoinCrowdsale, initialRate, finalRate, openingTime, closingTime)
+  deployer.deploy(DedcoinCrowdsale, initialRate, finalRate, openingTime, closingTime, rate, wallet, token)
 };
