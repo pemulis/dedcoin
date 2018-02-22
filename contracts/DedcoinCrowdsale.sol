8d4bf416cbde03d3968109e19fec00a515ab413c
John Shutt <john.d.shutt@gmail.com>
2018-02-22 11:59:01 -0800

    add _initialRate and _finalRate in the comments for DedcoinCrowdsale contract, and modify the contract to work as a DecreasingPriceCrowdsale

---
+++ contracts/DedcoinCrowdsale.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.18;
+
+import './Dedcoin.sol';
+import './DecreasingPriceCrowdsale.sol';
+
+
+contract DedcoinCrowdsale is DecreasingPriceCrowdsale {
+
+  function DedcoinCrowdsale(uint256 _initialRate, uint256 _finalRate) {
+    DecreasingPriceCrowdsale(_initialRate, _finalRate);
+    // _initialRate = 1/30000000000000000
+    // _finalRate = 1
+  }
+
+  // creates the token to be sold.
+  // override this method to have crowdsale of a specific MintableToken token.
+  function createTokenContract() internal returns (MintableToken) {
+    return new Dedcoin();
+  }
+
+}
