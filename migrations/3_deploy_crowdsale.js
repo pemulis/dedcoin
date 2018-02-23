4e28049fa3665046772cd9e81a8f219efc9457ff
John Shutt <john.d.shutt@gmail.com>
2018-02-22 16:30:15 -0800

    check in current migration code

--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -2,11 +2,11 @@ const DedcoinCrowdsale = artifacts.require("./DedcoinCrowdsale.sol")
 const Dedcoin = artifacts.require("./Dedcoin.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const initialRate = (1/30000000000000000)
-  const finalRate = 1
+  const initialRate = 1
+  const finalRate = 3
   const openingTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
   const closingTime = openingTime + (86400 * 20) // 20 days
-  const rate = (1/30000000000000000)
+  const rate = 1
   const wallet = accounts[0]
   const token = Dedcoin
 
